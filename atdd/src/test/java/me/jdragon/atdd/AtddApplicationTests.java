051cba1aaaa89b0def1aceff23327a7cf120bfcb
jdragon <koola97620@nate.com>
2020-01-17 09:14:35 +0900

    계정확인

---
+++ atdd/src/test/java/me/jdragon/atdd/AtddApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jdragon.atdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
