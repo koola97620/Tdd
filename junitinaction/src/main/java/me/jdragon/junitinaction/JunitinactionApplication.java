c06c45a2fad5f3396e2ee83bb19f23281d0dbf0e
Jaeyong <koola97620@nate.com>
2020-01-19 18:27:36 +0900

    JUnit in Action Chapter 1,2

---
+++ junitinaction/src/main/java/me/jdragon/junitinaction/JunitinactionApplication.java
@@ -0,0 +1,13 @@
+package me.jdragon.junitinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitinactionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JunitinactionApplication.class, args);
+  }
+
+}
---
+++ junitinaction/src/test/java/me/jdragon/junitinaction/JunitinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jdragon.junitinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitinactionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
