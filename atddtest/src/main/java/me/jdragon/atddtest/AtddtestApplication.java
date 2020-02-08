18d1e529512660c14f254d81da299aa10d737c2a
Jaeyong <koola97620@nate.com>
2020-02-08 22:16:19 +0900

    create

---
+++ atddtest/src/main/java/me/jdragon/atddtest/AtddtestApplication.java
@@ -0,0 +1,13 @@
+package me.jdragon.atddtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtddtestApplication.class, args);
+	}
+
+}
---
+++ atddtest/src/test/java/me/jdragon/atddtest/AtddtestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jdragon.atddtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
