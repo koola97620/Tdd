fe823a52f0fc5b8ff8037c8fed9213ef47e43491
Jaeyong <koola97620@nate.com>
2020-02-17 19:35:14 +0900

    initial commit

---
+++ atddsubway/src/test/java/me/jdragon/atddsubway/AtddsubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jdragon.atddsubway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddsubwayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
