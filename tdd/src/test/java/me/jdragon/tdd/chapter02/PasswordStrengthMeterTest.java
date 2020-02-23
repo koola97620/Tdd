bfc85f2c9e6e5f80ba0513bab670d09a74df97f5
Jaeyong <koola97620@nate.com>
2020-02-23 22:55:45 +0900

    chapter2 3. 숫자를 포함하지 않고 나머지 조건은 충족, 코드정리

--- tdd/src/test/java/me/jdragon/tdd/chapter02/PasswordStrengthMeterTest.java
+++ tdd/src/test/java/me/jdragon/tdd/chapter02/PasswordStrengthMeterTest.java
@@ -1,7 +1,6 @@
 package me.jdragon.tdd.chapter02;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,24 +10,27 @@ import org.junit.jupiter.api.Test;
  * @description
  */
 class PasswordStrengthMeterTest {
+  private PasswordStrengthMeter meter = new PasswordStrengthMeter();
   @Test
   public void meetsAllCriteira_Then_Strong() {
-    PasswordStrengthMeter passwordStrengthMeter = new PasswordStrengthMeter();
-    PasswordStrength result = passwordStrengthMeter.meter("abab2121");
-    assertThat(result).isEqualTo(PasswordStrength.STRONG);
-
-    PasswordStrength result2 = passwordStrengthMeter.meter("abc12311");
-    assertThat(result2).isEqualTo(PasswordStrength.STRONG);
+    assertStrength("abab2121" , PasswordStrength.STRONG);
+    assertStrength("abc12311" , PasswordStrength.STRONG);
   }
 
   @Test
-  public void meetsOtherCriteria_expect_for_Length_Then_Normal() {
-    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-    PasswordStrength result = meter.meter("ab12!@A");
-    assertThat(result).isEqualTo(PasswordStrength.NORMAL);
+  public void meetsOtherCriteria_except_for_Length_Then_Normal() {
+    assertStrength("ab12!@A" , PasswordStrength.NORMAL);
+    assertStrength("ABC12!" , PasswordStrength.NORMAL);
+  }
 
-    PasswordStrength result2 = meter.meter("ABC12!");
-    assertThat(result2).isEqualTo(PasswordStrength.NORMAL);
+  @Test
+  public void meetsOtherCriteria_except_for_number_Then_Normal() {
+    assertStrength("ab!@ABQWER" , PasswordStrength.NORMAL);
+  }
+
+  private void assertStrength(String password, PasswordStrength expStr) {
+    PasswordStrength result = meter.meter(password);
+    assertThat(result).isEqualTo(expStr);
   }
 
 }
