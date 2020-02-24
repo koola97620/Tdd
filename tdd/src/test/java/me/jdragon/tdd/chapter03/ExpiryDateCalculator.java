bc38aa034e4c87d860fe5a4e60f82123bcff9fcc
Jaeyong <koola97620@nate.com>
2020-02-25 04:45:57 +0900

    chapter3 다시 잘꺼야!

---
+++ tdd/src/test/java/me/jdragon/tdd/chapter03/ExpiryDateCalculator.java
@@ -0,0 +1,15 @@
+package me.jdragon.tdd.chapter03;
+
+import java.time.LocalDate;
+
+/**
+ * @author choijaeyong on 2020/02/24.
+ * @project tdd
+ * @description
+ */
+public class ExpiryDateCalculator {
+
+  public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+    return LocalDate.of(2020,4,1);
+  }
+}
--- tdd/src/test/java/me/jdragon/tdd/chapter03/ExpiryDateCalculatorTest.java
+++ tdd/src/test/java/me/jdragon/tdd/chapter03/ExpiryDateCalculatorTest.java
@@ -1,5 +1,7 @@
 package me.jdragon.tdd.chapter03;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +18,13 @@ public class ExpiryDateCalculatorTest {
 
   @DisplayName("만원 납부하면 한달 뒤가 만료일이 됨 ")
   @Test
-  public void give10000_and_3_1_Then_4_1_expiryDate() {
+  public void pay_10000_and_3_1_Then_4_1_expiryDate() {
     LocalDate billingDate = LocalDate.of(2020, 3, 1);
     int payAmount = 10_000;
+    ExpiryDateCalculator cal = new ExpiryDateCalculator();
+    LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+
+    assertThat(expiryDate).isEqualTo(LocalDate.of(2020,4,1));
 
 
   }
