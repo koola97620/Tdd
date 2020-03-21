3134c207e0dbef81b2132c41222aa03aaa91f762
Jaeyong <koola97620@nate.com>
2020-03-21 11:51:33 +0900

    팀장님 책 다시보기 chapter3 리팩토링 p 73

--- tdd2/src/test/java/chapter3/ExpiryDateCalculatorTest.java
+++ tdd2/src/test/java/chapter3/ExpiryDateCalculatorTest.java
@@ -14,23 +14,15 @@ public class ExpiryDateCalculatorTest {
 
   @Test
   public void 만원_납부하면_한달_뒤가_만료일이됨() {
-    LocalDate billingDate = LocalDate.of(2019, 3, 1);
-    int payAmount=10_000;
+    assertExpiryDate(LocalDate.of(2019,3,1) , 10_000, LocalDate.of(2019,4,1));
+    assertExpiryDate(LocalDate.of(2019,5,5) , 10_000, LocalDate.of(2019,6,5));
+  }
 
+  private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
     ExpiryDateCalculator cal = new ExpiryDateCalculator();
     LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
 
-    assertThat(expiryDate).isEqualTo(LocalDate.of(2019,4,1));
-
-    LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-    int payAmount2=10_000;
-
-    ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-    LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-
-    assertThat(expiryDate2).isEqualTo(LocalDate.of(2019,6,5));
-
-
+    assertThat(expiryDate).isEqualTo(expectedExpiryDate);
   }
 
 }
