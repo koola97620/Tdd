54c3a79fe264895aa8d3ff8d50438a9fd47ead88
Jaeyong <koola97620@nate.com>
2020-03-25 20:37:45 +0900

    팀장님 책 다시보기 chapter3 리팩토링 p 89

--- tdd2/src/test/java/chapter3/ExpiryDateCalculator.java
+++ tdd2/src/test/java/chapter3/ExpiryDateCalculator.java
@@ -1,6 +1,7 @@
 package chapter3;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 /**
  * @author choijaeyong on 2020/03/21.
@@ -14,12 +15,19 @@ public class ExpiryDateCalculator {
   }
 
   public LocalDate calculateExpiryDate(PayData payData) {
+    int addedMonths = payData.getPayAmount() / 10_000;
+
     if (payData.getFirstBillingDate() != null) {
-      LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+      LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
       if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+
+        if(YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
+          return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+        }
+
         return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
       }
     }
-    return payData.getBillingDate().plusMonths(1);
+    return payData.getBillingDate().plusMonths(addedMonths);
   }
 }
