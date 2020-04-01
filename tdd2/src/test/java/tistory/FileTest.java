419bb2b59ff36fa29ccaa2b88c04db656b3a9aaa
Jaeyong <koola97620@nate.com>
2020-04-01 23:38:14 +0900

    팀장님 책 다시보기 chapter6 까지 읽고 블로그 예제코드 작성중

--- tdd2/src/test/java/tistory/FileTest.java
+++ tdd2/src/test/java/tistory/FileTest.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,13 +17,22 @@ import org.junit.jupiter.api.Test;
  */
 public class FileTest {
 
+  private PayService payService;
+
+  @BeforeEach
+  public void setUp() {
+    payService = new PayService();
+  }
+
   @Test
-  public void fileTest() throws IOException {
-    String path = "/Users/jdragon/exam";
+  public void given_file_then_save() throws IOException {
+    String path = "/Users/jdragon/exam/pay/results";
     Stream<Path> fileList = Files.list(Paths.get(path));
+//    if(System.getProperty("os.name").toLowerCase().indexOf("win")>=0){
+//      targetDirPath = "c:"+ targetDirPath;
+//    }
     for (Path filePath :  fileList.collect(Collectors.toList())) {
-      // code
-      System.out.println(filePath);
+      payService.savePayResult(filePath);
     }
   }
 
---
+++ tdd2/src/test/java/tistory/PayService.java
@@ -0,0 +1,15 @@
+package tistory;
+
+import java.nio.file.Path;
+
+/**
+ * @author choijaeyong on 2020/04/01.
+ * @project tdd2
+ * @description
+ */
+public class PayService {
+
+  public void savePayResult(Path filePath) {
+
+  }
+}
